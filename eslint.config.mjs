5cb2a41f28c5d886110bf33a9b44fe63bb6eb549
Kinzangkin <franclinpondaag@gmail.com>
2025-05-29 12:22:00 +0800

    Disable no-unused-expressions rule for Prisma generated files in ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["src/generated/prisma/**"],
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  },
 ];
 
 export default eslintConfig;

5548e3d9c708b99864a3233756325baf149c47c1
Kinzangkin <franclinpondaag@gmail.com>
2025-05-29 12:32:39 +0800

    Disable additional ESLint rules for Prisma generated files to fix build errors on Vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,12 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-expressions": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
     },
   },
 ];
